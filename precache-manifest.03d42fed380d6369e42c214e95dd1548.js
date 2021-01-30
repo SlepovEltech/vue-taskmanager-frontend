dddd7742c723dfbae5d29c93ae895492c67f2e36
Artem Slepov <artemslepov01@gmail.com>
2021-01-30 12:58:36 +0300

    deploy

---
+++ precache-manifest.03d42fed380d6369e42c214e95dd1548.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2910097e0d904fb1bd45",
+    "url": "/vue-taskmanager-frontend/css/app.b9b46aeb.css"
+  },
+  {
+    "revision": "27639a7543adc90ff521",
+    "url": "/vue-taskmanager-frontend/css/chunk-04e27bfa.e6928dda.css"
+  },
+  {
+    "revision": "376d2a51282a1ed398d6",
+    "url": "/vue-taskmanager-frontend/css/chunk-vendors.24ce4a6c.css"
+  },
+  {
+    "revision": "754e15db78af997531db5862c7ab826f",
+    "url": "/vue-taskmanager-frontend/index.html"
+  },
+  {
+    "revision": "2910097e0d904fb1bd45",
+    "url": "/vue-taskmanager-frontend/js/app.2da75d9a.js"
+  },
+  {
+    "revision": "27639a7543adc90ff521",
+    "url": "/vue-taskmanager-frontend/js/chunk-04e27bfa.44ad3843.js"
+  },
+  {
+    "revision": "81bafcad01bfab2bff21",
+    "url": "/vue-taskmanager-frontend/js/chunk-32d867d6.79fb7cf1.js"
+  },
+  {
+    "revision": "01209a7499d3febd1c6a",
+    "url": "/vue-taskmanager-frontend/js/chunk-d94a5ba0.f549baa4.js"
+  },
+  {
+    "revision": "376d2a51282a1ed398d6",
+    "url": "/vue-taskmanager-frontend/js/chunk-vendors.817c71cf.js"
+  },
+  {
+    "revision": "20e5c2643ed929075b92f6e80c282815",
+    "url": "/vue-taskmanager-frontend/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-taskmanager-frontend/robots.txt"
+  }
+]);
